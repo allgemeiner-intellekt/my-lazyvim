bd76c497265abe2918166341e5bb184e3d69806e
allgemeiner-intellekt <mail.yuhan.li@gmail.com>
2026-05-20 13:32:46 +0100

    feat(editor): enable relative line numbers with absolute toggle in insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,22 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Use relative line numbers in Normal mode, and absolute line numbers in Insert mode.
+local number_group = vim.api.nvim_create_augroup("toggle_relative_number", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = number_group,
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = number_group,
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show absolute line number for current line, and relative line numbers in Normal mode.
+vim.opt.number = true
+vim.opt.relativenumber = true
